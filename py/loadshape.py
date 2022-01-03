2475c34410de65eaea1da3c553a978bc6e05e410
Jon <jtwilson@ashevillenc.gov>
2022-01-02 20:20:30 -0500

    something weird about thos buttons

--- py/loadshape.py
+++ py/loadshape.py
@@ -1,17 +1,11 @@
-import sys
 import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = '1'
-import pygame
-import math
-import random
 from lib.matrix import matrix_multiplication
 from math import acos,atan2,sqrt,cos,sin
 import importlib
 from os.path import dirname, basename, isfile, join
-import glob
 from lib.blit_text import blit_text
 def loadshape(filenm):
-    fieldOfView = 0.63
     shape = importlib.import_module("data." + filenm)
     points, lines = shape.points, shape.lines
     try: shape.z_distance
@@ -20,116 +14,5 @@ def loadshape(filenm):
     try: shape.drawdots
     except AttributeError: drawdots = False
     else: drawdots = shape.drawdots
+    return points, lines, z_distance, drawdots
 
-    #os.environ["SDL_VIDEO_CENTERED"]='1'  # Center window
-    black, white, blue  = (0, 0, 0), (230, 230, 230), (0, 154, 255)
-    scr_width, scr_height = 1680, 1050
-    # scr_width, scr_height = 3000, 2000 
-    pygame.init()
-    pygame.display.set_caption("Xspec")
-    screen = pygame.display.set_mode((scr_width, scr_height))
-    clock = pygame.time.Clock()
-    fps = 60
-    font = pygame.font.SysFont('Arial', 14)
-
-    anglex = 0
-    angley = 0
-    anglez = 0
-    speedx = 0
-    speedy = 0
-    speedz = 0
-    #z_distance = defaults["z_distance"]
-    scr_center = [scr_width//2, scr_height//2]
-    scale = scr_height * 2 // 3
-    speedup = 0.01
-
-    def connect_point(i, j, projected_points):
-        a = projected_points[i]
-        b = projected_points[j]
-        pygame.draw.line(screen, black, (a[0], a[1]), (b[0], b[1]), 1)
-
-    def getCameraCoords(x,y,z):
-        if x==y==z==0:
-            return [0,0]
-        x1 = (acos(-(z / sqrt(x**2 + y**2 + z**2)))) * fieldOfView * cos( atan2(y,x) )
-        y1 = (acos(-(z / sqrt(x**2 + y**2 + z**2)))) * fieldOfView * sin( atan2(y,x) )
-        return [x1,y1]
-
-    run = True
-    hidetext = False
-    while run:
-        clock.tick(fps)
-        screen.fill(white)
-        if not hidetext:
-            text = 'Use the arrow keys or WASD to rotate shape.' \
-            + '\nZ to zoom in, X to zoom out.' \
-            + '\nH to hide this text.' \
-            + '\nESC to quit.' \
-            + '\n' \
-            + '\nX Speed: ' + str(speedx) \
-            + '\nY Speed: ' + str(speedy) \
-            + '\nZ Distance: ' + str(z_distance)
-
-            blit_text(screen, text, (20,20), font)
-
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT or event.key == ord('a'):
-                    speedy = speedy + speedup
-                if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                    speedy = speedy - speedup
-                if event.key == pygame.K_UP or event.key == ord('w'):
-                    speedx = speedx + speedup
-                if event.key == pygame.K_DOWN or event.key == ord('s'):
-                    speedx = speedx - speedup
-                if event.key == ord('z'):
-                    z_distance = z_distance - 0.9
-                if event.key == ord('x'):
-                    z_distance = z_distance + 0.9
-                if event.key == ord('h'):
-                    hidetext = True
-            if event.type == pygame.KEYUP:
-                if event.key == ord('q') or event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-        index = 0
-        projected_points = [j for j in range(len(points))]
-
-        rotation_x = [[1, 0, 0],
-                    [0, math.cos(anglex), -math.sin(anglex)],
-                    [0, math.sin(anglex), math.cos(anglex)]]
-
-        rotation_y = [[math.cos(angley), 0, -math.sin(angley)],
-                    [0, 1, 0],
-                    [math.sin(angley), 0, math.cos(angley)]]
-
-        rotation_z = [[math.cos(anglez), -math.sin(anglez), 0],
-                    [math.sin(anglez), math.cos(anglez), 0],
-                    [0, 0 ,1]]
-
-        for point in points:
-            # rotate shape in 3d
-            rotated_2d = matrix_multiplication(rotation_x, point)
-            rotated_2d = matrix_multiplication(rotation_y, rotated_2d)
-            rotated_2d = matrix_multiplication(rotation_z, rotated_2d)
-            
-            # project to 2d
-            projected_2d = getCameraCoords(rotated_2d[0][0],rotated_2d[1][0],rotated_2d[2][0]-z_distance)
-            x = int(projected_2d[0] * scale) + scr_center[0]
-            y = int(projected_2d[1] * scale) + scr_center[1]
-
-            projected_points[index] = [x, y]
-            if drawdots:
-                pygame.draw.circle(screen, black, (x, y), 3)
-            index += 1
-        #draw edges       
-        for line in lines:
-            connect_point(line[0], line[1], projected_points)
-        
-
-        anglex += speedx
-        angley += speedy
-        anglez += speedz
-        pygame.display.update()
-
-    pygame.quit()
--- py/menu.py
+++ py/menu.py
@@ -2,11 +2,27 @@ import sys
 import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = '1'
 import pygame
+import math
 from os.path import dirname, basename, isfile, join
+from lib.matrix import matrix_multiplication
+from math import acos,atan2,sqrt,cos,sin
 import glob
+from lib.blit_text import blit_text
 import pygame_widgets
 from pygame_widgets.button import Button
 from loadshape import loadshape
+
+def connect_point(i, j, projected_points):
+    a = projected_points[i]
+    b = projected_points[j]
+    pygame.draw.line(screen, black, (a[0], a[1]), (b[0], b[1]), 1)
+
+def getCameraCoords(x,y,z):
+    if x==y==z==0:
+        return [0,0]
+    x1 = (acos(-(z / sqrt(x**2 + y**2 + z**2)))) * fieldOfView * cos( atan2(y,x) )
+    y1 = (acos(-(z / sqrt(x**2 + y**2 + z**2)))) * fieldOfView * sin( atan2(y,x) )
+    return [x1,y1]
 
 datadir = os.getcwd() + "/data"
 modules = glob.glob(join(datadir, "*.py"))
@@ -16,9 +32,27 @@ filelist = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('
 black, white, blue  = (0, 0, 0), (230, 230, 230), (0, 154, 255)
 scr_width, scr_height = 1680, 1050
 # scr_width, scr_height = 3000, 2000 
+fieldOfView = 0.63
+
+anglex = 0
+angley = 0
+anglez = 0
+speedx = 0
+speedy = 0
+speedz = 0
+scr_center = [scr_width//2, scr_height//2]
+scale = scr_height * 2 // 3
+speedup = 0.01
+points = []
+lines = []
+z_distance = 10
+drawdots = False
+
 pygame.init()
 pygame.display.set_caption("Xspec")
 screen = pygame.display.set_mode((scr_width, scr_height))
+clock = pygame.time.Clock()
+fps = 60
 font = pygame.font.SysFont('Arial', 14)
 
 screen.fill(white)
@@ -26,20 +60,89 @@ screen.fill(white)
 text = 'Enter a number to choose a file to load.'
 for ix, filenm in enumerate(filelist):
     Button(
-        screen,10,35*ix,180,30,text=filenm,
+        screen,10,35*ix+200,180,30,text=filenm,
         onClick=loadshape,
         onClickParams=[filenm])
 
 run = True
+hidetext = False
 while run:
+    clock.tick(fps)
+    screen.fill(white)
+    if not hidetext:
+        text = 'Choose a shape from the list below.' \
+        + 'Use the arrow keys or WASD to rotate shape.' \
+        + '\nZ to zoom in, X to zoom out.' \
+        + '\nH to hide this text.' \
+        + '\nESC to quit.' \
+        + '\n' \
+        + '\nX Speed: ' + str(speedx) \
+        + '\nY Speed: ' + str(speedy) \
+        + '\nZ Distance: ' + str(z_distance)
+
+        blit_text(screen, text, (20,20), font)
+
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.KEYDOWN:
-            print (chr(event.key))
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                speedy = speedy + speedup
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                speedy = speedy - speedup
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                speedx = speedx + speedup
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                speedx = speedx - speedup
+            if event.key == ord('z'):
+                z_distance = z_distance - 0.9
+            if event.key == ord('x'):
+                z_distance = z_distance + 0.9
+            if event.key == ord('h'):
+                hidetext = True
         if event.type == pygame.KEYUP:
             if event.key == ord('q') or event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+    index = 0
+    projected_points = [j for j in range(len(points))]
+
+    rotation_x = [[1, 0, 0],
+                [0, math.cos(anglex), -math.sin(anglex)],
+                [0, math.sin(anglex), math.cos(anglex)]]
+
+    rotation_y = [[math.cos(angley), 0, -math.sin(angley)],
+                [0, 1, 0],
+                [math.sin(angley), 0, math.cos(angley)]]
+
+    rotation_z = [[math.cos(anglez), -math.sin(anglez), 0],
+                [math.sin(anglez), math.cos(anglez), 0],
+                [0, 0 ,1]]
+
+    for point in points:
+        # rotate shape in 3d
+        rotated_2d = matrix_multiplication(rotation_x, point)
+        rotated_2d = matrix_multiplication(rotation_y, rotated_2d)
+        rotated_2d = matrix_multiplication(rotation_z, rotated_2d)
+        
+        # project to 2d
+        projected_2d = getCameraCoords(rotated_2d[0][0],rotated_2d[1][0],rotated_2d[2][0]-z_distance)
+        x = int(projected_2d[0] * scale) + scr_center[0]
+        y = int(projected_2d[1] * scale) + scr_center[1]
+
+        projected_points[index] = [x, y]
+        if drawdots:
+            pygame.draw.circle(screen, black, (x, y), 3)
+        index += 1
+    #draw edges       
+    for line in lines:
+        connect_point(line[0], line[1], projected_points)
+    
+
+    anglex += speedx
+    angley += speedy
+    anglez += speedz
+
+
     pygame_widgets.update(events)
     pygame.display.update()
 
